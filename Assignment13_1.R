b15eee3d255007aad14ae9c63d397f47caffc3c8
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-09-26 22:39:46 +0530

    Add files via upload

--- Assignment13_1.R
+++ Assignment13_1.R
@@ -19,28 +19,41 @@ library(rpart)
 library(rpart.plot)
 library(RColorBrewer)
 library(rattle)
+library(mice)
+library(VIM)
 
-
-df<-read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data", header = FALSE)
+df<-read.csv("F:/AcadGild/workings/COBRA-YTD2017-A13.csv")
 
 summary(df)
 str(df)
 dim(df)
 describe(df)
 head(df)
+View(df)
 sapply(df,class)
 
+# missing values imputation with  with median  
+sapply(df, function(x) sum(is.na(x))) # missing values  
+df$loc_type[is.na(df$loc_type)] <- median(df$loc_type, na.rm=TRUE)
+df$MaxOfnum_victims[is.na(df$MaxOfnum_victims)] <- median(df$MaxOfnum_victims, na.rm=TRUE)
+md.pattern(df)
+
+model<-lm(beat ~ MinOfucr+MaxOfnum_victims+x+y, data= df)
+model
+
+
 # correlation scatterplot for all the variables 
-plot(df$V2, df$V3)
-pairs(df)
+plot(df$MaxOfnum_victims, df$MinOfucr)
+plot(df$beat, df$MaxOfnum_victims)
+plot(df$beat, df$MinOfucr)
 
-cor(df[2:9], method = "spearman")
+cor(df[,c(8,12,15,22,23)], method = "spearman")
 
-cor.test(df$V2, df$V3)
-cor.test(df$V4, df$V5)
-cor.test(df$V6, df$V7)
-cor.test(df$V7, df$V8)
-cor.test(df$V8, df$V9)
+cor.test(df$beat, df$MaxOfnum_victims)
+cor.test(df$MaxOfnum_victims, df$x)
+cor.test(df$MaxOfnum_victims, df$y)
+cor.test(df$MaxOfnum_victims,df$MinOfucr)
+cor.test(df$x, df$y) 
 
 
 
